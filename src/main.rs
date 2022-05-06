6d01455642b52b21be47c0db36573fc9fd27818c
James Cole <james@assimityai.com>
2022-05-06 10:41:46 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+use actix_web::{middleware, web, App, HttpResponse, HttpServer, Result};
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "forecast.html")]
+struct ForecastTemplate<'a> {
+    forecast_name: &'a str,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct Index;
+
+async fn index(query: web::Query<HashMap<String, String>>) -> Result<HttpResponse> {
+    let s = if let Some(forecast_name) = query.get("forecast_name") {
+        ForecastTemplate { forecast_name }.render().unwrap()
+    } else {
+        Index.render().unwrap()
+    };
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/").route(web::get().to(index)))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
