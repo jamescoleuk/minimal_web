4092465c7504ca5dbe598c16add3a38f23cab587
James Cole <james@assimityai.com>
2022-05-09 08:06:45 +0100

    Add range adjustment

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use actix_files as fs;
-use actix_web::{middleware, web, App, HttpResponse, HttpServer, Result};
+use actix_web::{middleware, web, App, HttpRequest, HttpResponse, HttpServer, Result};
 use askama::Template;
 use chrono::{Duration, NaiveDate};
 use log::info;
+use serde::Deserialize;
 use std::collections::HashMap;
 
 #[derive(Template)]
@@ -20,8 +21,10 @@ struct ListTemplate<'a> {
 struct Range {
     start: NaiveDate,
     end: NaiveDate,
-    label: String, // TODO: this should be (<start> - <end> (<days> days))
+    label: String,
     value: i32,
+    can_ceil: bool,
+    can_floor: bool,
 }
 
 #[derive(Template)]
@@ -36,6 +39,17 @@ struct RangesTemplate<'a> {
 #[derive(Template)]
 #[template(path = "index.html")]
 struct Index;
+
+#[derive(Deserialize)]
+struct RangeFormData {
+    start_date: NaiveDate,
+    end_date: NaiveDate,
+    range_1: i32,
+    range_2: i32,
+    range_3: i32,
+    range_4: i32,
+    range_5: i32,
+}
 
 async fn index(query: web::Query<HashMap<String, String>>) -> Result<HttpResponse> {
     let s = if let Some(forecast_name) = query.get("forecast_name") {
@@ -53,6 +67,48 @@ async fn list() -> Result<HttpResponse> {
             "forecast 2".to_string(),
             "forecast 3".to_string(),
         ],
+    }
+    .render()
+    .unwrap();
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+async fn adjust_range(form: web::Form<RangeFormData>, req: HttpRequest) -> Result<HttpResponse> {
+    let mut range_values = vec![
+        form.range_1,
+        form.range_2,
+        form.range_3,
+        form.range_4,
+        form.range_5,
+    ];
+
+    let total = range_values.iter().sum::<i32>();
+    let mut adjustment = req
+        .headers()
+        .get("HX-Trigger-Name")
+        .expect("HX-Trigger-Name is necessary for me to know what range to apply this to.")
+        .to_str()
+        .unwrap()
+        .split('_');
+    let operation = adjustment.nth(0).unwrap();
+    let index_of_range_to_adjust = adjustment.next().unwrap().parse::<usize>().unwrap();
+    // TODO: index ranges by 0-based index, not 1-based index
+    let value_to_adjust = range_values.get(index_of_range_to_adjust - 1).unwrap();
+
+    let new_value = match operation {
+        "floor" => value_to_adjust - (total - 100),
+        "ceil" => value_to_adjust + (100 - total),
+        _ => panic!("Unknown adjustment operation: {}", operation),
+    };
+
+    range_values[index_of_range_to_adjust - 1] = new_value;
+
+    let ranges = get_ranges(form.start_date, form.end_date, &range_values);
+    let s = RangesTemplate {
+        ranges: &ranges,
+        start_date: form.start_date,
+        end_date: form.end_date,
+        total: &range_values.iter().sum(),
     }
     .render()
     .unwrap();
@@ -108,24 +164,29 @@ async fn update_ranges(query: web::Query<HashMap<String, String>>) -> Result<Htt
 
 fn get_ranges(start_date: NaiveDate, end_date: NaiveDate, range_values: &[i32]) -> Vec<Range> {
     let days_in_range = end_date.signed_duration_since(start_date).num_days();
-
     let number_of_ranges = 5;
-
     let range_size = days_in_range / number_of_ranges;
     let mut ranges: Vec<Range> = Vec::new();
-
     let mut range_start_date = start_date;
+    let total: i32 = range_values.iter().sum();
 
     for index in 0..number_of_ranges {
         let range_end_date = range_start_date
             .checked_add_signed(Duration::days(range_size))
             .unwrap();
         let days = (range_end_date - range_start_date).num_days();
+        let this_value = *range_values.get(index as usize).unwrap_or(&0);
+        let can_floor = total > 100 && this_value > 0;
+        let can_ceil = total < 100;
+        info!("{}, {}, {}, {}", total, this_value, can_floor, can_ceil);
+        let label = format!("{range_start_date} - {range_end_date} ({days} days)").to_string();
         ranges.push(Range {
             start: range_start_date,
             end: range_end_date,
-            label: format!("{range_start_date} - {range_end_date} ({days} days)").to_string(),
-            value: *range_values.get(index as usize).unwrap_or(&0),
+            label,
+            value: this_value,
+            can_floor,
+            can_ceil,
         });
         range_start_date = range_end_date
             .checked_add_signed(Duration::days(1))
@@ -164,6 +225,7 @@ async fn main() -> std::io::Result<()> {
             .service(web::resource("/list").route(web::get().to(list)))
             .service(web::resource("/generate_ranges").route(web::get().to(generate_ranges)))
             .service(web::resource("/update_ranges").route(web::get().to(update_ranges)))
+            .service(web::resource("/adjust_range").route(web::post().to(adjust_range)))
             .service(fs::Files::new("/static", "./static").show_files_listing())
     })
     .bind(("127.0.0.1", 8080))?
