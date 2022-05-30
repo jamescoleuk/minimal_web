aa682c865565fcf4cd57d45705326fb8d2bad65f
James Cole <james@assimityai.com>
2022-05-30 18:47:24 +0100

    Refactor back

--- src/forecasts/ui/range.rs
+++ src/forecasts/ui/range.rs
@@ -5,8 +5,6 @@ use log::info;
 use serde::Deserialize;
 use std::collections::HashMap;
 
-use super::get_ranges;
-
 pub struct Range {
     pub(crate) start: NaiveDate,
     pub(crate) end: NaiveDate,
@@ -132,4 +130,55 @@ pub async fn update_ranges(query: web::Query<HashMap<String, String>>) -> Result
     .render()
     .unwrap();
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+pub async fn create(query: web::Query<HashMap<String, String>>) -> Result<HttpResponse> {
+    let start_date_str = query.get("start_date").unwrap();
+    let end_date_str = query.get("end_date").unwrap();
+
+    let start_date = NaiveDate::parse_from_str(start_date_str, "%Y-%m-%d").unwrap();
+    let end_date = NaiveDate::parse_from_str(end_date_str, "%Y-%m-%d").unwrap();
+
+    let s = RangesTemplate {
+        ranges: &get_ranges(start_date, end_date, &[20, 20, 20, 20, 20]),
+        start_date,
+        end_date,
+        total: &100,
+    }
+    .render()
+    .unwrap();
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+pub fn get_ranges(start_date: NaiveDate, end_date: NaiveDate, range_values: &[i32]) -> Vec<Range> {
+    let days_in_range = end_date.signed_duration_since(start_date).num_days();
+    let number_of_ranges = 5;
+    let range_size = days_in_range / number_of_ranges;
+    let mut ranges: Vec<Range> = Vec::new();
+    let mut range_start_date = start_date;
+    let total: i32 = range_values.iter().sum();
+
+    for index in 0..number_of_ranges {
+        let range_end_date = range_start_date
+            .checked_add_signed(Duration::days(range_size))
+            .unwrap();
+        let days = (range_end_date - range_start_date).num_days();
+        let this_value = *range_values.get(index as usize).unwrap_or(&0);
+        let can_floor = total > 100 && this_value > 0;
+        let can_ceil = total < 100;
+        info!("{}, {}, {}, {}", total, this_value, can_floor, can_ceil);
+        let label = format!("{range_start_date} - {range_end_date} ({days} days)").to_string();
+        ranges.push(Range {
+            start: range_start_date,
+            end: range_end_date,
+            label,
+            value: this_value,
+            can_floor,
+            can_ceil,
+        });
+        range_start_date = range_end_date
+            .checked_add_signed(Duration::days(1))
+            .unwrap();
+    }
+    ranges
 }
